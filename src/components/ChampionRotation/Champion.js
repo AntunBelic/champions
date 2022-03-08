5cc6633081d5810c4c44217cc15439abb8e97cf3
AntunBelic <antunbelic93@gmail.com>
2022-03-08 20:40:59 +0100

    champion_selection_css

--- src/components/ChampionRotation/Champion.js
+++ src/components/ChampionRotation/Champion.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Col } from "react-bootstrap";
+import "./ChampionRotation.css";
 
 export default function Champion({ champion }) {
   return (
     <Col key={champion.info.key}>
-      <img
-        src={`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${champion.info.image.full}`}
-        alt={champion.info.id}
-      ></img>
+      <div className="champion__container">
+        <img
+          src={`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${champion.info.image.full}`}
+          alt={champion.info.id}
+          className="champion__img"
+        ></img>
+        <div className="champion__overlay champion__overlay--blur">
+          <h5 className="champion__title">{champion.info.id}</h5>
+        </div>
+      </div>
     </Col>
   );
 }
--- src/components/ChampionRotation/ChampionRotation.js
+++ src/components/ChampionRotation/ChampionRotation.js
@@ -3,13 +3,15 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export default function ChampionRotation({ champions }) {
   return (
-    <Container>
-      <Row>
+    <Container mx={"auto"} style={{ maxWidth: "740px" }} fluid>
+      <Row className="gy-5">
         <Col xs={{ span: 8, offset: 2 }}>
           <h1 style={{ textAlign: "center" }}>Champions rotation</h1>
         </Col>
       </Row>
-      <Row>{champions}</Row>
+      <Row xs={"auto"} md={5} className="justify-content-center gy-3 gx-1">
+        {champions}
+      </Row>
     </Container>
   );
 }
